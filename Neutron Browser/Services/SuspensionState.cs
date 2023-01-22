20ef8094a12a0962200fe55af620307b7f8c35e4
zhh13 <zhh13@STUPID-JEFFSU>
2023-01-22 20:49:06 +0800

    添加项目文件。

---
+++ Neutron Browser/Services/SuspensionState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Neutron_Browser.Services
+{
+    public class SuspensionState
+    {
+        public object Data { get; set; }
+
+        public DateTime SuspensionDate { get; set; }
+    }
+}
---
+++ Neutron Browser/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace Neutron_Browser.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
